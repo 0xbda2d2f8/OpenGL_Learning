0a09cca6afb0650e5f3a02abe8c48bdb56b13b95
xiaojian <hadesxjc@gmail.com>
2015-03-05 22:55:59 +0800

    test
    test

---
+++ OpenGL_Learn.cpp
@@ -0,0 +1,16 @@
+// OpenGL_Learn.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	system("pause");
+	return 0;
+}
+
